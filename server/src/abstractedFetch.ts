a77a5951bac680e4d11af985031e594c8bd20f81
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-06 19:38:59 +0000

    refactor(server): adapt fetch to receive options that configures the built in fetch

--- server/src/abstractedFetch.ts
+++ server/src/abstractedFetch.ts
@@ -1,4 +1,4 @@
 export const toJson = (response: Response) => response.json();
 
-export const abstractedFetch = (endpoint: string) =>
-  fetch(`${process.env.SERVICE_URL}/${endpoint}`).then(toJson);
+export const abstractedFetch = (endpoint: string, options?: RequestInit) =>
+  fetch(`${process.env.SERVICE_URL}/${endpoint}`, options).then(toJson);

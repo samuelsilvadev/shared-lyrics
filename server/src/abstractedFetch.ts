035be01e7534704e1be742c0f9771b8c2efd6574
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-07 09:02:11 +0000

    feat(server): create abstractedPost utility

--- server/src/abstractedFetch.ts
+++ server/src/abstractedFetch.ts
@@ -2,3 +2,15 @@ export const toJson = (response: Response) => response.json();
 
 export const abstractedFetch = (endpoint: string, options?: RequestInit) =>
   fetch(`${process.env.SERVICE_URL}/${endpoint}`, options).then(toJson);
+
+export const abstractedPost = (
+  endpoint: string,
+  body: Record<string, string | number | boolean>
+) =>
+  abstractedFetch(endpoint, {
+    body: JSON.stringify(body),
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+  }).then(toJson);

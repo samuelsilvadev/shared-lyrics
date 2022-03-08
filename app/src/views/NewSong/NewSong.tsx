a11d7705fca821650bf917a4174c64b3972947af
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-08 22:34:33 +0000

    feat(app): add a heading on the new song component

--- app/src/views/NewSong/NewSong.tsx
+++ app/src/views/NewSong/NewSong.tsx
@@ -1,5 +1,5 @@
 import { FormEventHandler, useState } from "react";
-import { Button, FormLabel, Input, Box } from "@chakra-ui/react";
+import { Button, FormLabel, Input, Box, Heading } from "@chakra-ui/react";
 import { gql, useMutation } from "@apollo/client";
 
 const SAVE_SONG_MUTATION = gql`
@@ -30,19 +30,24 @@ function NewSong() {
   };
 
   return (
-    <Box as="form" p="4" onSubmit={handleOnSubmit}>
-      <FormLabel htmlFor="song-name">Song title:</FormLabel>
-      <Input
-        id="song-name"
-        type="text"
-        value={songTitle}
-        onChange={({ target: { value } }) => setSongTitle(value)}
-        marginBlockEnd="2"
-      />
-      <Button variant="solid" colorScheme="yellow" type="submit">
-        Save
-      </Button>
-    </Box>
+    <>
+      <Heading as="h1" marginBlockStart="4" marginInline="4">
+        Create a new song
+      </Heading>
+      <Box as="form" p="4" onSubmit={handleOnSubmit}>
+        <FormLabel htmlFor="song-name">Song title:</FormLabel>
+        <Input
+          id="song-name"
+          type="text"
+          value={songTitle}
+          onChange={({ target: { value } }) => setSongTitle(value)}
+          marginBlockEnd="2"
+        />
+        <Button variant="solid" colorScheme="yellow" type="submit">
+          Save
+        </Button>
+      </Box>
+    </>
   );
 }
 

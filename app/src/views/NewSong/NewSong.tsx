cb350b28806ec6a2f981b83f00db4af6f41d565c
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-08 22:26:46 +0000

    feat(app): save a new song using `useMutation`

--- app/src/views/NewSong/NewSong.tsx
+++ app/src/views/NewSong/NewSong.tsx
@@ -1,5 +1,49 @@
+import { FormEventHandler, useState } from "react";
+import { Button, FormLabel, Input, Box } from "@chakra-ui/react";
+import { gql, useMutation } from "@apollo/client";
+
+const SAVE_SONG_MUTATION = gql`
+  mutation SaveSong($title: String!) {
+    addSong(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+type SaveSongsMutationResponse = {
+  addSong: { title: string; id: string };
+};
+
 function NewSong() {
-  return <>New Song</>;
+  const [saveSong] = useMutation<SaveSongsMutationResponse>(SAVE_SONG_MUTATION);
+  const [songTitle, setSongTitle] = useState("");
+
+  const handleOnSubmit: FormEventHandler = (event) => {
+    event.preventDefault();
+
+    saveSong({
+      variables: {
+        title: songTitle,
+      },
+    });
+  };
+
+  return (
+    <Box as="form" p="4" onSubmit={handleOnSubmit}>
+      <FormLabel htmlFor="song-name">Song title:</FormLabel>
+      <Input
+        id="song-name"
+        type="text"
+        value={songTitle}
+        onChange={({ target: { value } }) => setSongTitle(value)}
+        marginBlockEnd="2"
+      />
+      <Button variant="solid" colorScheme="yellow" type="submit">
+        Save
+      </Button>
+    </Box>
+  );
 }
 
 export default NewSong;

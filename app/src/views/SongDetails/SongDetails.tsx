84e95169f7210c6c44f4f982e62f749cef94dfae
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-09 23:43:08 +0000

    feat(app): retrieve specific song details

--- app/src/views/SongDetails/SongDetails.tsx
+++ app/src/views/SongDetails/SongDetails.tsx
@@ -1,7 +1,45 @@
+import { gql, useQuery } from "@apollo/client";
 import { Button, Heading } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+const GET_SONG = gql`
+  query GetSong($id: ID!) {
+    song(id: $id) {
+      id
+      title
+      lyrics {
+        id
+        content
+        likes
+      }
+    }
+  }
+`;
+
+type GetSongQueryResponse = {
+  song: {
+    id: string;
+    title: string;
+    lyrics: {
+      id: string;
+      content: string;
+      likes: number;
+    }[];
+  } | null;
+};
 
 function SongDetails() {
+  const { id: songId } = useParams<{ id: string }>();
+  const { loading, data } = useQuery<GetSongQueryResponse>(GET_SONG, {
+    variables: {
+      id: songId,
+    },
+  });
+
+  if (loading) {
+    return <>Loading...</>;
+  }
+
   return (
     <>
       <Button
@@ -14,7 +52,7 @@ function SongDetails() {
         Back
       </Button>
       <Heading as="h1" marginBlockStart="4">
-        Song Details
+        {data?.song?.title}
       </Heading>
     </>
   );
